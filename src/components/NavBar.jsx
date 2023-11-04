f24422614aae2f723f9fd3741c58df524c75fd4c
Codder12346768 <dharmip060@gmail.com>
2023-11-04 08:36:08 +0530

    product details page updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import hamburger from '../assets/hamburger.svg';
-import cross from '../assets/cross.svg'
-import account from '../assets/account.svg'
-// import {account} from '../assets/account.svg' 
+import cross from '../assets/cross.svg';
+import account from '../assets/account.svg';
+
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="bg-primary w-full">
       <nav className="">
@@ -15,23 +16,22 @@ function NavBar() {
               <div className="flex-shrink-0">
                 {/* <img
                   className="h-8 w-8"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                  src={account}
                   alt="Workflow"
                 /> */}
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4 text-xl">
-                
                   <a
                     href="#"
-                    className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md  font-medium"
+                    className="hover:bg-gray-700 text-white px-3 py-2 rounded-md font-medium"
                   >
-                   Home
+                    Home
                   </a>
 
                   <a
                     href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md  font-medium"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
                   >
                     Cart
                   </a>
@@ -49,44 +49,30 @@ function NavBar() {
                   >
                     Account
                   </a>
-
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md font-medium"
-                  >
-                    Account
-                  </a>
-                                  <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Account
-                </a>
-                  {/* <div className="relative">
-                  <img src={account} style={{ width: '200px', height: '200px' }} alt="account" className="ml-10 p-2" />
-                  </div> */}
-                  
                 </div>
               </div>
             </div>
-            <div className="-mr-2 flex md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <img src={hamburger} alt="hamburger" />
-                ) : (
+            <div className="flex items-center">
+              <div className="flex items-center">
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  type="button"
+                  className="md:hidden bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                  style={{ marginLeft: "10px" }} // Add this line to align to the left corner
+                >
+                  <span className="sr-only">Open main menu</span>
+                  {!isOpen ? (
+                    <img src={hamburger} alt="hamburger" className="" />
+                  ) : (
                     <img src={cross} alt="cross" />
-                )}
-              </button>
-              <button className="relative">
-                <img src={account} alt="account" className="absolute top-2 left-2" />
-              </button>
+                  )}
+                </button>
+
+              </div>
+
+              <img src={account} alt="account" className="ml-4" />
             </div>
           </div>
         </div>
@@ -103,50 +89,38 @@ function NavBar() {
           {(ref) => (
             <div className="md:hidden" id="mobile-menu">
               <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-gray-300 hover:bg-gray-700 hover-text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Home
                 </a>
 
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-gray-300 hover-bg-gray-700 hover-text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Cart
                 </a>
 
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-gray-300 hover-bg-gray-700 hover-text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Product
                 </a>
 
                 <a
                   href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-gray-300 hover-bg-gray-700 hover-text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Account
                 </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Account
-                </a>
-            
-                
               </div>
             </div>
           )}
         </Transition>
       </nav>
-
-      
-
     </div>
   );
 }
