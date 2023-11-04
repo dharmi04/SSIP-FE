5f15a1974d70ae4c6bc7f794f73d348b7474ac2e
Codder12346768 <dharmip060@gmail.com>
2023-11-04 10:18:00 +0530

    CSS

--- src/components/Vase.jsx
+++ src/components/Vase.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Vase = (props) => {
   return (
-    <div className=' rounded-md bg-grey2 p-3'>
-      <img src={props.img} alt="products" />
-        <h4>{props.name}</h4>
-        <p>{props.description}</p>
-        <h4>{props.price}</h4>
+    <div className='rounded-md bg-grey2 p-4 mb-4'>
+      <div className="text-center w-64 h-72">
+        <img src={props.img} alt="products" className="w-52 h-52 md:mx-auto mb-2 p-4" />
+        <h4 className="text-lg font-semibold">{props.name}</h4>
+        <p className="text-sm p-2">{props.description}</p>
+        <h4 className="text-accent mt-2 p-2">${props.price}</h4>
+      </div>
     </div>
   )
 }
