fc4a467f9ae692e643a8d5fdf72a855fc2efff6d
Preet Sojitra <preet.dev373@gmail.com>
2023-11-15 19:26:15 +0530

    fix: scroll position was not preserving

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Explore from './Pages/Explore';
-import Home from './Pages/Home';
-import Productpage from './Pages/Productpage';
-import Productdetail from './Pages/Productdetail';
-import Dashboard from './Pages/Dashboard';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Arpage from './Pages/Arpage';
-import Account from './Pages/Account';
-import AddProduct from './Pages/AddProduct'
-import BuyNow from './Pages/BuyNow';
-import Cart from './Pages/Cart';
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Explore from "./Pages/Explore"
+import Home from "./Pages/Home"
+import Productpage from "./Pages/Productpage"
+import Productdetail from "./Pages/Productdetail"
+import Dashboard from "./Pages/Dashboard"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
+import Arpage from "./Pages/Arpage"
+import Account from "./Pages/Account"
+import AddProduct from "./Pages/AddProduct"
+import BuyNow from "./Pages/BuyNow"
+import Cart from "./Pages/Cart"
 
+import ScrollToTop from "./ScrollToTop"
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Explore />} />
         <Route path="/home" element={<Home />} />
@@ -26,13 +28,13 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/ar" element={<Arpage />} />
-        <Route path="/account" element={<Account />}  />
-        <Route path="/addproduct" element={ <AddProduct/> } />
-        <Route path="/buynow" element={ <BuyNow /> } />
-        <Route path="/cart" component={ <Cart />  } />
+        <Route path="/account" element={<Account />} />
+        <Route path="/addproduct" element={<AddProduct />} />
+        <Route path="/buynow" element={<BuyNow />} />
+        <Route path="/cart" component={<Cart />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
